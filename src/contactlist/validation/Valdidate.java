961782dcb00bd28c19a0522b3e7bdc653eddb12a
admin@atul <atulsingh29@protonmail.com>
2020-02-24 20:29:29 +0530

    validate class added

---
+++ src/contactlist/validation/Valdidate.java
@@ -0,0 +1,7 @@
+package contactlist.validation;
+
+public class Valdidate {
+    String email="^([a-zA-Z0-9_\\.]+)@([a-zA-Z0-9_\\.]+)\\.([a-zA-Z]{2,5})$";
+    String mobile="^[0][1-9]\\d{9}$|^[1-9]\\d{9}$";
+
+}
