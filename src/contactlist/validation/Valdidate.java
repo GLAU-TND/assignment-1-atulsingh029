b0437f0ef70350b0547e6f8612f363f39ffc82c8
admin@atul <atulsingh29@protonmail.com>
2020-02-24 20:34:24 +0530

    emailVal and numberVal method defination added

--- src/contactlist/validation/Valdidate.java
+++ src/contactlist/validation/Valdidate.java
@@ -4,4 +4,13 @@ public class Valdidate {
     String email="^([a-zA-Z0-9_\\.]+)@([a-zA-Z0-9_\\.]+)\\.([a-zA-Z]{2,5})$";
     String mobile="^[0][1-9]\\d{9}$|^[1-9]\\d{9}$";
 
+    public String emailVal(String input){
+        String output="";
+        return output;
+    }
+
+    public String numberVal(String input){
+        String output="";
+        return output;
+    }
 }
